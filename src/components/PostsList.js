730608f78ffc1ba0e7fc29eb610a2eceda562876
Fagner <frodriguesbetim@gmail.com>
2023-02-05 12:10:33 -0300

    feat: initial commit

---
+++ src/components/PostsList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostList = () => (
+  <h2>PostList</h2>
+);
+
+export default PostList;
---
+++ src/pages/visitant.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Visitant = () => (
+  <h2>Pagina Visitante</h2>
+);
+
+export default Visitant;
